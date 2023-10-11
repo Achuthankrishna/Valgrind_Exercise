e2d06896532904bf98208252d7621c9b2db55132
Achuthankrishna <achkrish07@gmail.com>
2023-10-11 19:38:15 -0400

    Rectified memory leak and updated valgrind text

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -17,7 +17,11 @@ int AnalogSensor::Read()
 {
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
+    //valgrind leak says- memory leak since the new file is created and not hashed after execution
+
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    //We detele the new vector after executiin to prevent memory leak
+    delete(readings);
     return result;
 }
 
